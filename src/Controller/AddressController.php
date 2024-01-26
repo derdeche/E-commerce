9e826dbbbb92c8cb5633ca7600abe6aa21e90de2
derdeche <foued.derdeche@gmail.com>
2024-01-26 11:30:33 +0100

    add adresscontroller

---
+++ src/Controller/AddressController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Address;
+use App\Form\AddressType;
+use App\Repository\AddressRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/mon-compte/adresse')]
+class AddressController extends AbstractController
+{
+    #[Route('/', name: 'app_address_index', methods: ['GET'])]
+    public function index(AddressRepository $addressRepository): Response
+    {
+        $user = $this->getUser();
+        $addresses = $addressRepository->findBy(['user' => $user]);
+        return $this->render('address/index.html.twig', [
+            'addresses' => $addresses,
+        ]);
+    }
+
+    #[Route('/ajouter', name: 'app_address_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $address = new Address();
+        $user = $this->getUser();
+    
+        if (!$user instanceof User) {
+            // Gérer le cas où $user n'est pas une instance de la classe User
+            // Peut-être rediriger vers une page d'erreur, par exemple
+            // ...
+    
+            // Exemple de redirection vers une page d'erreur
+            return $this->redirectToRoute('app_error');
+        }
+    
+        $address->setUser($user);
+    
+        $form = $this->createForm(AddressType::class, $address);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($address);
+            $entityManager->flush();
+    
+            // Après avoir persisté l'adresse, on récupére l'ID de l'utilisateur
+            $userId = $user->getId();
+
+            $this->addFlash('success', 'Votre Adresse a été ajoutée avec succès.');
+            $this->addFlash('success-class', 'hide-message');
+
+            // Redirection vers la page app_user avec l'ID de l'utilisateur
+            return $this->redirectToRoute('app_user', ['id' => $userId], Response::HTTP_SEE_OTHER);
+        }
+    
+        return $this->render('address/new.html.twig', [
+            'address' => $address,
+            'form' => $form,
+        ]);
+    }
+    
+    
+
+    #[Route('/{id}', name: 'app_address_show', methods: ['GET'])]
+    public function show(Address $address): Response
+    {
+        return $this->render('address/show.html.twig', [
+            'address' => $address,
+        ]);
+    }
+
+    // #[Route('/{id}/modifier-mon-adresse', name: 'app_address_edit', methods: ['GET', 'POST'])]
+    // public function edit(Request $request, Address $address, EntityManagerInterface $entityManager): Response
+    // {
+    //     $form = $this->createForm(AddressType::class, $address);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->flush();
+
+    //         return $this->redirectToRoute('app_address_index', [], Response::HTTP_SEE_OTHER);
+    //     }
+
+    //     return $this->render('address/edit.html.twig', [
+    //         'address' => $address,
+    //         'form' => $form,
+    //     ]);
+    // }
+
+    #[Route('/{id}', name: 'app_address_delete', methods: ['POST'])]
+    public function delete(Request $request, Address $address, EntityManagerInterface $entityManager): Response
+    {
+        $userId = $address->getUser()->getId(); // on récupére l'ID de l'utilisateur avant la suppression
+    
+        if ($this->isCsrfTokenValid('delete'.$address->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($address);
+            $entityManager->flush();
+        }
+        $this->addFlash('success', 'Votre Adresse a été supprimée avec succès.');
+        $this->addFlash('success-class', 'hide-message');
+        // on redirige vers la page app_user avec l'ID de l'utilisateur
+        return $this->redirectToRoute('app_user', ['id' => $userId], Response::HTTP_SEE_OTHER);
+    }
+    
+}
