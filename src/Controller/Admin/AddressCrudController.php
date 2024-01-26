c3a089bd5e82ea7ce2a71dc28e1c6f076750dadf
derdeche <foued.derdeche@gmail.com>
2024-01-26 11:36:55 +0100

    add adresscrud controller

---
+++ src/Controller/Admin/AddressCrudController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Address;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class AddressCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Address::class;
+    }
+
+    public function configureActions(Actions $actions): Actions{
+        return $actions 
+        ->add(Crud::PAGE_EDIT, Action::INDEX)
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->add(Crud::PAGE_EDIT, Action::DETAIL);
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name' , 'Address Name'),
+            TextField::new('client_name'),
+            ChoiceField::new('address_type')->setChoices(['Livraison' => 'Livraison', 'Facturation' => 'Facturation']),
+            TextField::new('street'),
+            TextField::new('code_postal'),
+            TextField::new('city'),
+            TextField::new('state'),
+            TextEditorField::new('more_details' ),
+            AssociationField::new('user'),
+
+        ];
+    }
+    
+}
