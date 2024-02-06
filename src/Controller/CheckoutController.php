99970485db7e27cd98951e71bd44a5166f6a3c32
derdeche <foued.derdeche@gmail.com>
2024-02-06 15:10:35 +0100

    add redirection to home

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -186,6 +186,14 @@ public function orderDetails($id): Response
     ]);
 }
 
+ // une redirection vers la page home si un code est introduit dans
+    /**
+     * @Route("/{any}", name="redirect_home", requirements={"any"=".+"})
+     */
+    public function redirectHome(): Response
+    {
+        return $this->redirectToRoute('app_home');
+    }
     
 
  
