7dd6537fc2cb0df8b0df893599e7b11b4dc1b15a
derdeche <foued.derdeche@gmail.com>
2024-02-06 14:47:36 +0100

    add script to cartItemcount

---
+++ public/assets/js/header.js
@@ -0,0 +1,18 @@
+
+// on appele la fonction pour la première fois lors du chargement de la page
+document.addEventListener('DOMContentLoaded', updateCartItemCount);
+
+// On utilise AJAX pour récupérer le nombre d'articles dans le panier et mettre à jour l'icône du panier
+function updateCartItemCount() {
+    fetch('/panier/nombre') 
+        .then(response => response.json())
+        .then(data => {
+            const cartCountElement = document.getElementById('cartItemCount');
+            if (cartCountElement) {
+                cartCountElement.innerText = data.count;
+            }
+        })
+        .catch(error => console.error('Erreur lors de la récupération du nombre d\'articles dans le panier', error));
+}
+
+
