ba714981e6cbd6fd69c55aaf456e0038234f7df4
derdeche <foued.derdeche@gmail.com>
2024-02-07 22:19:54 +0100

    migration

--- migrations/Version20240202105607.php
+++ migrations/Version20240202105607.php
@@ -20,12 +20,10 @@ final class Version20240202105607 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE address ADD CONSTRAINT FK_D4E6F81A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE address DROP FOREIGN KEY FK_D4E6F81A76ED395');
     }
 }
